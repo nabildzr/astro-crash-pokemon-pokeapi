ff40e998dfc164c70a93ab5f025e2176c028abaf
nabildzr <muhammadnbillz@gmail.com>
2025-01-29 05:10:56 +0700

    Add Vercel adapter and update static paths for Pokémon data fetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel({})
 });
